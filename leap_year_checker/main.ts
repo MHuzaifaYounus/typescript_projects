d7f1808dac004fb8811e1a35609e5e083b6c1aaf
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-03-03 20:04:21 +0500

    leap year checker

---
+++ leap_year_checker/main.ts
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+
+let leap = 4;
+let answer = await inquirer.prompt([{
+    name: "year",
+    type: "number",
+    message: ("Enter the year: ")
+}]);
+
+let n = answer.year % leap;
+
+if (n == 0) {
+    console.log(`${answer.year} is the Leap Year`)
+}
+
+else {
+    console.log(`${answer.year} is not the Leap Year`)
+
+}
+
