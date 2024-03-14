ec11ea1b9a9aed7de9e356a4aaab5d16dd277e3c
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-03-15 00:49:09 +0500

    uploading bmi calculator

---
+++ BMI_Calculator/main.ts
@@ -0,0 +1,22 @@
+// Body mass Index Calculator 
+import inquirer from "inquirer";
+// Taking Weight from user 
+let weightInKg = await inquirer.prompt({
+    name: "weight",
+    type: "number",
+    message: "Enter Your Weight In Kg: "
+})
+// Taking heigth from user 
+let heightInMeters = await inquirer.prompt({
+    name: "height",
+    type: "number",
+    message: "Enter Your Height In Meters: "
+})
+// Calculations
+let bmi: number = weightInKg.weight / (heightInMeters.height ** 2);
+// Printing Body Mass Index According to provided data by user 
+console.log(`Your Body Mass Index (BMI) is: ${bmi}` + " kg/m2");
+
+
+
+
