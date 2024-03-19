8fea4717188584e3e66847b6a4eb394aff5d5318
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-03-20 00:22:16 +0500

    updating simple calculator and adding ss

--- simple calculator/index.js
+++ simple calculator/index.js
@@ -1,28 +1,32 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const answer = await inquirer.prompt([
     { message: "Enter first number", type: "number", name: "firstnumber" },
     { message: "Select one of the operators to perform action", type: "list", name: "operator", choices: ["+", "-", "*", "/", "**", "%"] },
     { message: "Enter second number", type: "number", name: "secondnumber" },
 ]);
-if (answer.operator === "+") {
-    console.log(`Answer : ${answer.firstnumber} + ${answer.secondnumber}  =  ${answer.firstnumber + answer.secondnumber}`);
+if (isNaN(answer.firstnumber || answer.secondnumber)) {
+    console.log(chalk.red("PLz Enter the Valid Number"));
+}
+else if (answer.operator === "+") {
+    console.log(chalk.blue(`Answer : ${answer.firstnumber} + ${answer.secondnumber}  = `), chalk.green(`${answer.firstnumber + answer.secondnumber}`));
 }
 else if (answer.operator === "-") {
-    console.log(`Answer : ${answer.firstnumber} - ${answer.secondnumber}  =  ${answer.firstnumber - answer.secondnumber}`);
+    console.log(chalk.blue(`Answer : ${answer.firstnumber} - ${answer.secondnumber}  = `), chalk.green(`${answer.firstnumber - answer.secondnumber}`));
 }
 else if (answer.operator === "*") {
-    console.log(`Answer : ${answer.firstnumber} * ${answer.secondnumber}  =  ${answer.firstnumber * answer.secondnumber}`);
+    console.log(chalk.blue(`Answer : ${answer.firstnumber} * ${answer.secondnumber}  = `), chalk.green(`${answer.firstnumber * answer.secondnumber}`));
 }
 else if (answer.operator === "/") {
-    console.log(`Answer : ${answer.firstnumber} / ${answer.secondnumber}  =  ${answer.firstnumber / answer.secondnumber}`);
+    console.log(chalk.blue(`Answer : ${answer.firstnumber} / ${answer.secondnumber}  = `), chalk.green(`${answer.firstnumber / answer.secondnumber}`));
 }
 else if (answer.operator === "**") {
-    console.log(`Answer : ${answer.firstnumber} ** ${answer.secondnumber}  =  ${answer.firstnumber ** answer.secondnumber}`);
+    console.log(chalk.blue(`Answer : ${answer.firstnumber} ** ${answer.secondnumber}  = `), chalk.green(`${answer.firstnumber ** answer.secondnumber}`));
 }
 else if (answer.operator === "%") {
-    console.log(`Answer : ${answer.firstnumber} % ${answer.secondnumber}  =  ${answer.firstnumber % answer.secondnumber}`);
+    console.log(chalk.blue(`Answer : ${answer.firstnumber} % ${answer.secondnumber}  = `), chalk.green(`${answer.firstnumber % answer.secondnumber}`));
 }
 else {
-    console.log("Please select valid operator");
+    console.log(chalk.red("Please select valid operator"));
 }
