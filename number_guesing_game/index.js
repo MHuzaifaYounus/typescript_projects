fe8773438ce038a8c907df6aa1afe38e96eb56f4
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-03-24 23:09:01 +0500

    fixing bugs

--- number_guesing_game/index.js
+++ number_guesing_game/index.js
@@ -2,12 +2,11 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 let randomNumber;
-let running;
+let running = true;
 async function thinking() {
     return new Promise((resolve, reject) => {
         // generating random number 
         randomNumber = Math.round(Math.random() * 99 + 1);
-        running = true;
         console.log("Wait Bot is thinking a number... ");
         setTimeout(() => {
             console.log("Bot is ready with his number now you have to guess this number");
@@ -31,6 +30,7 @@ async function main() {
             console.log("WOW! You", chalk.green("Win"), "The Number Is " + chalk.green(randomNumber));
             console.log(`You Did it in only ${chalk.yellow(10 - index)} chances`);
             running = false;
+            break;
         }
         else if (answers.usernum < randomNumber) {
             console.log("Your Number Is ", chalk.blue("Less"), " than the Bot's Number ");
@@ -44,6 +44,7 @@ async function main() {
         // checking the remainig chances 
         if (index == 0) {
             console.log(`You ${chalk.red("Loose")}, The Number Is ${chalk.green(randomNumber)}`);
+            running = false;
         }
         else {
             console.log(`You have ${chalk.red(index)} chance remaining`);
--- number_guesing_game/index.ts
+++ number_guesing_game/index.ts
@@ -3,13 +3,13 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 
 let randomNumber: number;
-let running: boolean;
+let running: boolean = true;
 
 async function thinking() {
     return new Promise<void>((resolve, reject) => {
         // generating random number 
         randomNumber = Math.round(Math.random() * 99 + 1)
-        running = true;
+      
         console.log("Wait Bot is thinking a number... ");
         setTimeout(() => {
             console.log("Bot is ready with his number now you have to guess this number");
@@ -19,6 +19,7 @@ async function thinking() {
 }
 async function main() {
     await thinking()
+ 
     for (let index = 9; running; index--) {
         // taking input from user 
         const answers = await inquirer.prompt([
@@ -33,8 +34,9 @@ async function main() {
         if (answers.usernum == randomNumber) {
             console.log("WOW! You", chalk.green("Win"), "The Number Is " + chalk.green(randomNumber));
             console.log(`You Did it in only ${chalk.yellow(10 - index)} chances`);
-            
             running = false
+            break
+
         }
         else if (answers.usernum < randomNumber) {
             console.log("Your Number Is ", chalk.blue("Less"), " than the Bot's Number ");
@@ -49,10 +51,12 @@ async function main() {
         // checking the remainig chances 
         if (index == 0) {
             console.log(`You ${chalk.red("Loose")}, The Number Is ${chalk.green(randomNumber)}`);
+            running = false
+            
+            
         }
-        else {
+        else {            
             console.log(`You have ${chalk.red(index)} chance remaining`);
-
         }
 
 
